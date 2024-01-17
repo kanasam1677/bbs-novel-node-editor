bdfc2068d43a002fce39aa57eed561b7c50cdf8e
kanasam1677 <98017291+kanasam1677@users.noreply.github.com>
2024-01-17 15:57:59 +0900

    ノードの作成

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -4,20 +4,25 @@ import { useBaklava } from "@baklavajs/renderer-vue";
 import "@baklavajs/themes/dist/syrup-dark.css";
 import HelloWorld from './components/HelloWorld.vue'
 
+import ReplyNode from "./components/ReplyNode";
+
 window.electronAPI.sendMessage('Hello from App.vue!');
 const baklava = useBaklava();
+baklava.editor.registerNodeType(ReplyNode);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!--
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
+  -->
   <div style="width: 90vw; height: 90vh">
     <baklavaEditor :view-model="baklava" />
   </div>
