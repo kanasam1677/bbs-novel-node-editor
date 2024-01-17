6856222055050d0eb7d8978eab0f3ba481728efd
kanasam1677 <98017291+kanasam1677@users.noreply.github.com>
2024-01-17 18:35:49 +0900

    長さなどをカスタマイズするため、継承によるノード作成に変更

--- src/renderer/components/ReplyNode.ts
+++ src/renderer/components/ReplyNode.ts
@@ -1,14 +1,47 @@
 
-import { defineNode, NodeInterface, NumberInterface, SelectInterface } from "baklavajs";
+import { Node, NodeInterface, CalculateFunction, NumberInterface, SelectInterface } from "baklavajs";
 
-export default defineNode({
-    type: "ReplyNode",
-    inputs: {
-        number1: () => new NumberInterface("Number", 1),
-        number2: () => new NumberInterface("Number", 10),
-        operation: () => new SelectInterface("Operation", "Add", ["Add", "Subtract"]).setPort(false),
-    },
-    outputs: {
-        output: () => new NodeInterface("Output", 0),
-    },
-});
+interface Inputs {
+    number1: number;
+    number2: number;
+    operation: string;
+}
+
+interface Outputs {
+    output: number;
+}
+
+export default class ReplyNode extends Node<Inputs, Outputs> {
+    public type = "ReplyNode";
+
+    width = 400;
+
+    public inputs = {
+        number1: new NumberInterface("Number", 1),
+        number2: new NumberInterface("Number", 2),
+        operation: new SelectInterface("Operation", "Add", ["Add", "Subtract"]).setPort(false),
+    };
+
+    public outputs = {
+        output: new NodeInterface("Output", 0),
+    };
+
+    public constructor() {
+        super();
+        this.title = this.type;
+        outerWidth = 400;
+        this.initializeIo();
+    }
+
+    public calculate: CalculateFunction<Inputs, Outputs> = ({ number1, number2, operation }) => {
+        let output;
+        if (operation === "Add") {
+            output = number1 + number2;
+        } else if (operation === "Subtract") {
+            output = number1 - number2;
+        } else {
+            throw new Error("Unknown operation: " + operation);
+        }
+        return { output };
+    }
+}
